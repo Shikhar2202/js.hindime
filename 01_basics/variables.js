1e49bde623981a2ffb1b45978feb815d048ba573
Shikhar2202 <147388632+Shikhar2202@users.noreply.github.com>
2024-02-28 07:14:53 +0000

    variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -6,5 +6,5 @@ accountState="Bihar"
 console.table([accountId,accountCity,accountState])
 
 /* prefer not to use var
-because of issue in block scope and functional scope
+because of issue in block scope and functional scope.
 */
