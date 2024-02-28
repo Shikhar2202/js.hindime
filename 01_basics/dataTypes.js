7db84ea0b09c9f1893362b10d6fc4088c8759ba1
Shikhar2202 <147388632+Shikhar2202@users.noreply.github.com>
2024-02-28 07:12:54 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version.
+
+//alert(3+3) // we are using nodeJS not browser
+
+let name = "shikhar"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -4,3 +4,7 @@ let accountState="UP"
 accountCity="varanasi"
 accountState="Bihar"
 console.table([accountId,accountCity,accountState])
+
+/* prefer not to use var
+because of issue in block scope and functional scope
+*/
