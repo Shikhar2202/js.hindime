e0bba055e5a2744800f897bdb2ada4694144b9f3
Shikhar2202 <147388632+Shikhar2202@users.noreply.github.com>
2024-02-29 09:37:02 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperator.js
@@ -0,0 +1,48 @@
+/*let score = 33
+
+console.log(typeof(score));
+
+let valueInString = String(score)
+
+console.log(typeof valueInString);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "shikhar" => true
+
+*/
+
+
+
+// ********************  OPERATIONS  **********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = " shikhar"
+let str2 = "hello"
+
+console.log(str2 + str1);
+
+console.log("1" + 2);      // 12
+console.log(1 + "2");      // 12
+console.log("1" + 2 + 2);  // 122
+console.log(1 + 2 + "2");  // 32
+
+console.log(true);  // true
+console.log(+true); // 1
+console.log(+"");   // 0
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
