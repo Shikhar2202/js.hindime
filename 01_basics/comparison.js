faa1277fc9931b4ed28e35720c7bac6b1090eab2
Shikhar2202 <147388632+Shikhar2202@users.noreply.github.com>
2024-02-29 09:57:50 +0000

    comparison of datatypes

---
+++ 01_basics/comparison.js
@@ -0,0 +1,11 @@
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+// equality check and comparison check works differently
+
+// strict check (===)
+console.log("2" === 2);
