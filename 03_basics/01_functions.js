f04cdfb80c8574e0fe340d174bc47ee0cc8bdaa7
Shikhar2202 <147388632+Shikhar2202@users.noreply.github.com>
2024-03-05 12:02:45 +0000

    hi

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+
+
+function sayMyName (){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("K");
+    console.log("H");
+    console.log("A");
+    console.log("R");
+}
+
+sayMyName()
+
+function addTwoNumbers (num1, num2){        
+    console.log(num1 + num2); 
+}
+
+addTwoNumbers(3, 5);
