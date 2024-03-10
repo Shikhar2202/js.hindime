84e4db999b5e0406a9cc1efd2febe487f4bb9ce4
Shikhar2202 <147388632+Shikhar2202@users.noreply.github.com>
2024-03-10 06:39:10 +0000

    scope level and mini hosting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,12 +1,56 @@
-let a = 100
-const b = 200
+// let a = 100
+// const b = 200
+// var c = 300
 
-if (true){
-    let a = 10
-    const b = 20
-    var c = 30
+// if (true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c); // var is not good.
+
+// function one(){
+//     const username = "shikhar"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+
+//     two();
+// }
+
+// one();
+
+if (true) {
+    const username = "shikhar";
+
+    if(username === "shikhar"){
+        const website = "youtube"
+        // console.log(username + " " + website);
+    }
+    // console.log(website);
+
 }
 
-console.log(a);
-console.log(b);
-// console.log(c); // var is not good.
+// console.log(username);
+
+// ************************ INTERESTING *************************
+
+console.log(addone(5));
+function addone (num){
+    return num + 1;    // functon addOne can be access before initialization.
+}
+
+
+
+const addTwo = function(num){
+    return num + 2;           // function addTwo cannot be access before initialization.
+}
+
+console.log(addTwo(5));
+
