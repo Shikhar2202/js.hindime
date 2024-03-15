8083a5658a71150a1d6a34b53df663004544701d
Shikhar2202 <147388632+Shikhar2202@users.noreply.github.com>
2024-03-15 11:14:27 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// if
+const isUserLoggedIn = true;
+
+// if (2 == "2"){
+//     console.log(`executed`); // true and print executed.
+// }
+
+// if (2 === "2"){
+//     console.log(`executed`); // false because === also check type.
+// }
+
+// if (2 !== "2"){
+//     console.log(`executed`); // !== is opposite of ===.
+// }
+
+const balance = 1000;
+
+// if (balance > 500) console.log("test") , console.log(`balance is ${balance}`);
+
+// if(condition){
+
+// } else if(condition){
+
+// } else{
+
+// }
+
+
